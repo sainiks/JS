687601e77f54a046c627fe9862ecb0df8ad7b3b4
Kunal Saini <66879424+sainiks@users.noreply.github.com>
2024-07-14 11:05:22 +0000

    learn about Variables,Const,Let,etc.

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,18 @@
+const accountId =144553
+let accountEmail= "kunalsaini20090360@gmail.com"
+var accountPassword= "12345"
+accountCity ="Jaipur" // you can also reverse this type varibles in code without defineing it in the memmory
+let accountState;
+// accountId = 2 // not allowed
+
+accountEmail ="poonam@gmail.com"
+accountPassword = "98765"
+accountCity="Delhi"
+
+
+/*
+Prefer not to use var
+Because of issue in Block scope and functional scope
+
+*/ 
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
