fc6ae72aabd24d76f55d8884a73cb29875309f7e
Kunal Saini <66879424+sainiks@users.noreply.github.com>
2024-07-14 18:35:45 +0000

    Learn Different Types Of DataTypes

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; // Treat all JS code as newer version
+
+// alert( 3 + 3) //we are using Node JS not in browser console
+
+console.log(3 + 3 )  // code readablity should be high
+console.log("kunal")
+
+let name ="kunal"      // String datatype 
+let age = 19           // number datatype
+let isLoggedIn =false  // Boolean datatype
+let state  = null
+let city;
+
+
+// Number  => 2^53 range
+// Bigint => used for larger dataset numbers
+// String => 2^16 range and it uses " "
+// Boolean => 2^32 range and it gives only True/False 
+// Null => 2^32 range and it gives only Null it's standalone value 
+// Undefined => 2^32 range and it gives only Undefined
+// Symbol => 2^32 range and it gives unique it's standalone value
+
+
+// Object 
+// Object => 2^32 range and it gives only Object it's standalone value
+// typeof => is used identify the type of any variable 
+console.log(typeof"kunal");       // string datatype 
+console.log(typeof age);          // number datatype 
+console.log(typeof isLoggedIn);   // boolean datatype
+console.log(typeof null);         // object datatype 
+console.log(typeof undefined);    // undefined datatype 
