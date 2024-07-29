45ffc1c403634d797185a05c48ea3d49af3c7762
Kunal Saini <66879424+sainiks@users.noreply.github.com>
2024-07-29 17:37:17 +0000

    Details on Conversion & Operations

--- 01_Basics/03_ConversionOperation.js
+++ 01_Basics/03_ConversionOperation.js
@@ -29,5 +29,51 @@ let booleanIsLoggedIn= Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+//************************* Operations *************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+
+let str1 = "hello"
+let str2 = " kunal"
+let str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log("1" + 2 );
+// console.log(1 + "2");
+// console.log("1" + 2 + 2 );
+// console.log(1 + 2 + "2");
+// 
+// console.log(((3 + 4) * 5) % 3 );
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+let num1 , num2 , num3 
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+++gameCounter; // prefix here value is taken for use 100 and then incremented 101
+gameCounter++; // postfix here value is firstly incremented from 101 to 102 and then use.
+console.log(gameCounter);
+
+// link to study prefix and postfix from mdn web docs
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+// link to study type conversion
+// https://tc39.es/ecma262/multipage/abstract-operations.html
