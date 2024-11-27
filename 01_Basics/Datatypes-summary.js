daf0a0c9b9c136563231c938d30de304a032ff6f
Kunal Saini <66879424+sainiks@users.noreply.github.com>
2024-11-27 19:13:16 +0000

    Datatypes-Summary Explained in details

---
+++ 01_Basics/Datatypes-summary.js
@@ -0,0 +1,50 @@
+/*
+Primitive
+----------
+Their ar 7 types of primitive data types in JavaScript.
+1. Number
+2. String
+3. Boolean
+4. Null
+5. Undefined
+6. Symbol
+7. BigInt
+*/
+
+const score =100
+const scoreValue =100.3
+
+const isLoggedIn =false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 3456543576654356754n
+
+/*
+Reference Type (Non-Primitive)
+-------------------------------
+1. Array
+2. Object
+3. Function
+*/
+
+const heros = ["shaktiman" , "naagraj","doga"]
+
+let myObj={
+    name:"kunal",
+    age: 20,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof myFunction);
+
+//For refering to the Documentation of JavaScript
+// https://262.ecma-international.org/5.1/#sec-11.4.3
