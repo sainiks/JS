f0ae24bd8c4363803681f054e17ad84154834f39
Kunal Saini <66879424+sainiks@users.noreply.github.com>
2024-11-28 19:18:37 +0000

    Detailed Explanation on Strings

---
+++ 01_Basics/05_Strings.js
@@ -0,0 +1,40 @@
+const name = "kunal"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value"); 
+// This is old way of doing things in JavaScript we don't perfer this way
+
+console.log(`My name is ${name} and my repo count is ${repoCount} `); 
+// This is the new way of doing things in JavaScript we perfer this way
+// This method is called string interpolation
+
+
+const gameName = new String('kunal-ks-com')
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// 
+// 
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('s'));
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const anotherString = gameName.slice(-7,4)
+console.log(anotherString);
+
+const newStringOne = "   kunal   "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+
+const url = "https://kunal.com/kunal%20saini"
+
+console.log(url.replace('%20','-'));
+console.log(url.includes('kunal'));
+console.log(url.includes('aashi'));
+
+
+console.log(gameName.split('-'));
