77b9201ed96328c2eaa8d6dc0ecdb1e3999d1465
Kunal Saini <66879424+sainiks@users.noreply.github.com>
2025-05-19 19:44:52 +0000

    falsy and truthy values as well as differnece b/w nullish coalescing operator  and ternary operator

---
+++ 04_Control_flow/truthy.js
@@ -0,0 +1,42 @@
+// const userEmail = []
+
+// if(userEmail){
+    // console.log("Got your email")
+// }else{
+    // console.log("Don't have user email");
+    // 
+// }
+
+
+// falsy values
+// false , 0 , -0 ,bigInt 0n ,"" , null , undefined , NaN(not a number)
+
+// truthy values
+// "0", 'false'," ",[], {} , function(){} ,true , 1 , -1 , bigInt 1n , Symbol() , BigInt(0) , new Date() , Infinity
+
+// if (userEmail.length ===0 ){
+    // console.log("Array is empty");
+// }
+
+// const emptyobj = {}
+// 
+// if (Object.keys(emptyobj).length === 0 ){
+    // console.log("Object is empty");
+// }
+
+
+// Nullish coalescing operator (??) >> null or undefined 
+// let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15 
+// val1 = null ?? 10 ?? 20 
+// console.log(val1);
+
+
+// Ternary operator (?)
+// ------------------------
+// condition ? true : false 
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("Ice tea is less than 80"):console.log("Ice tea is more than 80")
