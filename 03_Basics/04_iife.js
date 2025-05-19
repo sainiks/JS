30c118a4957ae16a758874c1bccd0d6dbe93daa1
Kunal Saini <66879424+sainiks@users.noreply.github.com>
2025-05-19 19:42:09 +0000

    IIFE

---
+++ 03_Basics/04_iife.js
@@ -0,0 +1,19 @@
+// Immediatel Invoked Function Expression(IIFE)
+// IIFE is a function that runs as soon as it is defined
+
+
+//Regular function Example
+
+// function chai (){
+    // console.log(`DB Connected`);
+// }
+// 
+// chai();
+
+// IIFE Example
+// ()()
+//this parenthesis is used for wrapping the function ()()//this parenthesis is used for definiation of the function 
+(function chaiaurcode(){
+    console.log(`DB Connected again `)
+})()
+
